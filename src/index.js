89ba372a777acee9ec9bdf992113a873669797f9
Krish Majhi <krishmajhi9003@gmail.com>
2026-07-05 10:43:56 +0530

    index js,html added in src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./src/App.jsx";
-import foodStore from "./src/store/Store.jsx";
+import App from "./App.jsx";
+import foodStore from "./store/Store.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
